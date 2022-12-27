167aaa575bd09667d90ef1366b81ecb248e5efff
captv89 <vishnu.raveendranathan@gmail.com>
2022-12-27 23:37:45 +0100

    added golangGeojsonDijkstra package for the calculation

--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/kylelemons/go-gypsy v1.0.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
+	github.com/pitchinnate/golangGeojsonDijkstra v0.0.1 // indirect
+	github.com/tomchavakis/turf-go v0.0.3 // indirect
 	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	geo "github.com/kellydunn/golang-geo"
-	"github.com/twpayne/go-geom/encoding/geojson"
-	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/multi"
-	"gonum.org/v1/gonum/graph/path"
-	"hash/fnv"
+	gdj "github.com/pitchinnate/golangGeojsonDijkstra"
+	"io"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
+// Sample data for test Shanghai-New-York
+var originCoords = gdj.Position{121.8, 31.2}
+var destinationCoords = gdj.Position{-74.1, 40.7}
+
 func main() {
+
+	f, err := os.OpenFile("runtime.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	wrt := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(wrt)
+
+	var fc gdj.FeatureCollection
+
 	// Read the contents of the GeoJSON file into a byte slice
 	data, err := os.ReadFile("marnet_densified_v2.geojson")
 	if err != nil {
@@ -22,192 +33,46 @@ func main() {
 	}
 
 	//Unmarshall feature collection from geojson
-	collection := geojson.FeatureCollection{}
-	err = collection.UnmarshalJSON(data)
+	err = json.Unmarshal(data, &fc)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
-	// Print the number of features in the collection
-	log.Printf("Number of features: %d", len(collection.Features))
+	//// Print the number of features in the collection
+	//log.Printf("Number of features: %d", len(fc.Features))
 
-	// Create the graph
-	log.Println("Creating graph...")
-	g := CreateGraph(collection)
+	// Calculate the shortest path between two points
+	path, distance, err := fc.FindPath(originCoords, destinationCoords, 0.00001)
 
-	//	 Capture the start and end coordinates
-	fromLat := "35.5074"
-	fromLng := "0.1278"
-	toLat := "-23.5074"
-	toLng := "30.1278"
+	distanceInKm := distance / 1000
 
-	// Convert the from and to coordinates to floats
-	floatFromLat, err := strconv.ParseFloat(strings.TrimSpace(fromLat), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	floatFromLng, err := strconv.ParseFloat(strings.TrimSpace(fromLng), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	floatToLat, err := strconv.ParseFloat(strings.TrimSpace(toLat), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	floatToLng, err := strconv.ParseFloat(strings.TrimSpace(toLng), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Get the first and last coordinates of the path
+	lastWp := path[0]
+	firstWp := path[len(path)-1]
 
-	// Calculate the shortest distance between the start and end coordinates
-	distance := shortestDistance(g, floatFromLat, floatFromLng, floatToLat, floatToLng)
-	fmt.Println("Shortest distance:", distance)
+	log.Println("First waypoint: ", firstWp)
+	log.Println("Last waypoint: ", lastWp)
+
+	// Calculate the gc distance between the origin coordinates and the first waypoint
+	distToFirstWp := CalcDistance(originCoords, firstWp)
+	// Calculate gc distance between the destination coordinates and the last waypoint
+	distFromLastWp := CalcDistance(destinationCoords, lastWp)
+	// Total distance of the path
+	totalDistance := distToFirstWp + distanceInKm + distFromLastWp
+
+	// Print the path and distance
+	log.Printf("Waypoints: %v", path)
+	log.Printf("Origin to First Waypoint: %f Km", distToFirstWp)
+	log.Printf("Waypoint Distance: %f Km", distanceInKm)
+	log.Printf("Last Waypoint to Destination: %f Km", distFromLastWp)
+	log.Printf("Total Distance: %f Km", totalDistance)
+
 }
 
-// NewGeoNode creates a new node with the given ID, latitude, and longitude
-func NewGeoNode(id int64, lat, lng float64) *GeoNode {
-	return &GeoNode{
-		ID:  id,
-		Lat: lat,
-		Lng: lng,
-	}
-}
+// CalcDistance calculates the distance between two points in meters
+func CalcDistance(p1, p2 gdj.Position) float64 {
+	wp1 := geo.NewPoint(p1[1], p1[0])
+	wp2 := geo.NewPoint(p2[1], p2[0])
 
-func (n *GeoNode) GeoNode() {}
-
-func CreateGraph(collection geojson.FeatureCollection) *multi.WeightedDirectedGraph {
-	g := multi.NewWeightedDirectedGraph()
-
-	// Add nodes to the graph
-	for _, feature := range collection.Features {
-
-		coords := feature.Geometry.FlatCoords()
-
-		//fmt.Println(coords)
-		//fmt.Println("\n Length of coords", len(coords))
-
-		// For each coordinate, add a node to the graph
-		for i := 0; i < len(coords); i += 2 {
-
-			cnX := coords[i]
-			cnY := coords[i+1]
-
-			// Combine cnX and cnY into a single string and hash it to create a unique ID
-			cnID := hash(strconv.Itoa(int(cnX)) + strconv.Itoa(int(cnY)))
-
-			// Add the node to the graph
-			node1 := &GeoNode{ID: cnID, Lat: cnX, Lng: cnY}
-			g.AddNode(multi.Node(node1))
-
-			//fmt.Printf("cnID: %d, cnX: %f, cnY: %f", cnID, cnX, cnY)
-
-			// Check if node already exists in graph
-			if g.Node(cnID) == nil {
-				// Add node to graph
-				g.AddNode(multi.Node(cnID))
-			} else {
-				//fmt.Println("Node already exists")
-				g.Node(cnID)
-			}
-
-			nxCoords := coords[i+2:]
-			//fmt.Println("\nLength of nxCoords: ", len(nxCoords))
-			// Add edges to the graph edge adder
-			for node := 2; node <= len(nxCoords); node += 2 {
-				nxX := coords[i+node]
-				nxY := coords[i+node+1]
-
-				// Combine nxX and nxY into a single string and hash it to create a unique ID
-				nxID := hash(strconv.Itoa(int(nxX)) + strconv.Itoa(int(nxY)))
-
-				//fmt.Printf("\nnxID: %d, nxX: %f, nxY: %f", nxID, nxX, nxY)
-				//Storing the node ID as a string
-				g.From(cnID)
-				g.To(nxID)
-
-				// Calculate the distance between the two nodes
-				point1 := geo.NewPoint(coords[i], coords[i+1])
-				point2 := geo.NewPoint(coords[node], coords[node+1])
-				dist := point1.GreatCircleDistance(point2)
-
-				//fmt.Println("\tDistance:", dist)
-
-				// Add the edge to the graph
-				g.NewWeightedLine(multi.Node(cnID), multi.Node(nxID), dist)
-				g.NewWeightedLine(multi.Node(nxID), multi.Node(cnID), dist)
-			}
-		}
-
-	}
-
-	return g
-}
-
-func hash(s string) int64 {
-	h := fnv.New64a()
-	_, err := h.Write([]byte(s))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return int64(h.Sum64())
-}
-
-// Calculate the shortest distance between two coordinates
-func shortestDistance(g graph.WeightedDirected, fromLat, fromLng, toLat, toLng float64) float64 {
-	// Convert the from and to coordinates to points
-	fromPoint := geo.NewPoint(fromLat, fromLng)
-	toPoint := geo.NewPoint(toLat, toLng)
-
-	// Find the nearest node to the from and to points
-	fromNode := nearestNode(g, fromPoint)
-	toNode := nearestNode(g, toPoint)
-
-	// Convert toNode to int64
-	toNodeInt64 := toNode.ID()
-
-	// Use Dijkstra's algorithm to find the shortest path between the nodes
-	p := path.DijkstraFrom(fromNode, g)
-
-	// Return the distance of the shortest path
-	return p.WeightTo(toNodeInt64)
-}
-
-// Find the nearest node to a point
-func nearestNode(g graph.WeightedDirected, p *geo.Point) graph.Node {
-	var nearestNode graph.Node
-	minDistance := float64(1000000000)
-
-	// Iterate over all nodes in the graph
-	allNodes := g.Nodes()
-
-	for allNodes.Next() {
-		n := allNodes.Node()
-		lat, lng, err := nodeCoords(n)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		point := geo.NewPoint(lat, lng)
-		distance := point.GreatCircleDistance(p)
-
-		if distance < minDistance {
-			minDistance = distance
-			nearestNode = n
-		}
-	}
-
-	return nearestNode
-}
-
-// Get the latitude and longitude of a node
-func nodeCoords(n graph.Node) (float64, float64, error) {
-	lat, err := strconv.ParseFloat(strings.Split(fmt.Sprint(n), ",")[0], 64)
-	if err != nil {
-		return 0, 0, err
-	}
-	lng, err := strconv.ParseFloat(strings.Split(fmt.Sprint(n), ",")[1], 64)
-	if err != nil {
-		return 0, 0, err
-	}
-	return lat, lng, nil
+	return wp1.GreatCircleDistance(wp2)
 }
